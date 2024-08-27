f2ad05ce2a4aa4eb8440349a6e87b2c325782f59
Inam05 <inamulhassan.tech@gmail.com>
2024-08-27 09:52:39 +0500

    Descending Insertion sort

---
+++ LAB/Lab-2/Tasks/descendingInsertionSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] < key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = {44, 15, 35, 12, 74, 1, 5, 58};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    cout << "Insertion sort: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
