155dc604a59750d79ec4fcf4b830887c36edbbf2
Inam05 <inamulhassan.tech@gmail.com>
2024-09-02 11:28:16 +0500

    problem-1 solved

---
+++ Theory/Week-3/problem-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int *ptr;
+    int x;
+    ptr = &x;
+    cout << "x=" << x << endl;
+    cout << "*ptr=" << *ptr << endl;
+    *ptr += 5;
+    cout << "x=" << x << endl;
+    cout << "*ptr=" << *ptr << endl;
+    (*ptr)++;
+    cout << "x=" << x << endl;
+    cout << "*ptr=" << *ptr << endl;
+    return 0;
+}
