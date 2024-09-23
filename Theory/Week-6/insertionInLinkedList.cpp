c2635fd9cbecf34ecc4324c423b4ea0f8c1b3bc7
Inam05 <inamulhassan.tech@gmail.com>
2024-09-23 11:12:40 +0500

    Insertion in a linked list

---
+++ Theory/Week-6/insertionInLinkedList.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+    LinkedList()
+    {
+        head = nullptr;
+    }
+
+    void addAtBeginning(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void addAtEnd(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = nullptr;
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    void addAtPosition(int value, int pos)
+    {
+        if (pos == 1)
+        {
+            addAtBeginning(value);
+            return;
+        }
+        Node *newNode = new Node();
+        newNode->data = value;
+        Node *temp = head;
+        for (int i = 1; i < pos - 1 && temp != nullptr; i++)
+        {
+            temp = temp->next;
+        }
+        if (temp == nullptr)
+            return;
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    LinkedList list;
+    int choice, value, pos;
+    cout << "Where do you want to add the new node?\n1. Beginning\n2. End\n3. Specific position\n";
+    cin >> choice;
+    cout << "Enter value: ";
+    cin >> value;
+    if (choice == 1)
+    {
+        list.addAtBeginning(value);
+    }
+    else if (choice == 2)
+    {
+        list.addAtEnd(value);
+    }
+    else if (choice == 3)
+    {
+        cout << "Enter position: ";
+        cin >> pos;
+        list.addAtPosition(value, pos);
+    }
+    list.display();
+    return 0;
+}
